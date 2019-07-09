18312eb998d92804e9fe5338a3ed7bc19aedb90e
kacperg1999 <50631273+kacperg1999@users.noreply.github.com>
2019-07-10 00:12:08 +0200

    Graficzna reprezentacja kolejki

--- cDraw_Locomotive.h
+++ cDraw_Locomotive.h
@@ -4,16 +4,19 @@
 
 class cDraw_Locomotive {
 	double x_, y_;
+	double angle_;
+	int color_;
+	std::vector <int> passengers_;
 
 public:
 
-	cDraw_Locomotive(double x, double y);
+	cDraw_Locomotive(double x, double y, double angle);
 	void draw_locomotive();
-	void add_wagon();
-	void draw_passenger(char type);
+	void draw_passengers();
+
 	void change_position();
 	void rotate(float angle);
 	void set_color(double r, double g, double b);
-	bool is_clicked(double openglX, double openglY);
-	bool is_unclicked(double openglX, double openglY);
+	//bool is_clicked(double openglX, double openglY);
+	//bool is_unclicked(double openglX, double openglY);
 };
--- cDraw_Station.cpp
+++ cDraw_Station.cpp
@@ -10,43 +10,6 @@ cDraw_Station::cDraw_Station(double x, double y, std::vector <int> passengers, i
 
 }
 
-
-//void cDraw_Station::draw_station() 
-//{
-//glPushMatrix();
-//	glTranslated(x_/2, y_/2, 0.0);
-//	glRotated(0, 1.0, 0.0, 0.0);
-//	glRotated(0, 0.0, 1.0, 0.0);
-//	glRotated(0, 0.0, 0.0, 1.0);
-//
-//	glColor3d(1.0, 0.0, 0.0);
-//
-//
-//	double angle, vectorX, vectorY, vectorX1, vectorY1;
-//	glBegin(GL_TRIANGLES);
-//	vectorX1 = x_ / 2;
-//	vectorY1 = y_ / 2;
-//	int seg = 360;
-//	for (int i = 0; i <= seg; i++)
-//	{
-//		angle = (M_PI / (180.0))*((float)i);
-//		vectorX = x_ / 2 + (0.3*sin(angle));
-//		vectorY = y_ / 2 + (0.3*cos(angle));
-//		glVertex2f(x_ / 2, y_ / 2);
-//		glVertex2f(vectorX1, vectorY1);
-//		glVertex2f(vectorX, vectorY);
-//		vectorY1 = vectorY;
-//		vectorX1 = vectorX;
-//	}
-//	glEnd();
-//	glPopMatrix();
-//	glPushMatrix();
-//	glTranslated(x_, y_, 0.0);
-//
-//		this->draw_passengers();
-//
-//	glPopMatrix();
-//}
 void cDraw_Station::draw_station()
 {
 	switch (shape_)
